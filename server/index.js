c29baf097a0d0a91e80752889658d1827375053c
Suman Kayal <suman13kayal@gmail.com>
2026-05-23 02:36:04 +0530

    url

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
+      'https://final-gyaanmate-edu.vercel.app',
       'http://localhost:5173',
-      'https://final-gyaanmate-edu.vercel.app'
     ],
     credentials: true,
   })
